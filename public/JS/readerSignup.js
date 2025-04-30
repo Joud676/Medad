1048864c22de097aa849776e2f0d6e639f5bee9b
Joud Batarfi <139700859+Joud676@users.noreply.github.com>
2025-04-30 08:23:40 +0300

    Update readerSignup.js

--- public/JS/readerSignup.js
+++ public/JS/readerSignup.js
@@ -2,13 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: process.env.APIKEY,  // GitHub Secret FIREBASE_APIKEY
-    authDomain: process.env.AUTHDOMAIN,  // GitHub Secret FIREBASE_AUTHDOMAIN
-    projectId: process.env.PROJECTID,  // GitHub Secret FIREBASE_PROJECTID
-    storageBucket: process.env.STORAGEBUCKET,  // GitHub Secret FIREBASE_STORAGEBUCKET
-    messagingSenderId: process.env.MESSAGINGSENDERID,  // GitHub Secret FIREBASE_MESSAGINGSENDERID
-    appId: process.env.APPID,  // GitHub Secret FIREBASE_APPID
+
+const firebaseConfig = { 
+  apiKey: "${APIKEY}",  // GitHub Secret FIREBASE_APIKEY
+  authDomain: "${AUTHDOMAIN}",  // GitHub Secret FIREBASE_AUTHDOMAIN
+  projectId: "${PROJECTID}",  // GitHub Secret FIREBASE_PROJECTID
+  storageBucket: "${STORAGEBUCKET}",  // GitHub Secret FIREBASE_STORAGEBUCKET
+  messagingSenderId: "${MESSAGINGSENDERID}",  // GitHub Secret FIREBASE_MESSAGINGSENDERID
+  appId: "${APPID}",  // GitHub Secret FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
