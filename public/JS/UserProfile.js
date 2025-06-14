579248a6e9a786cdf9e236bc952e180913e7e6c6
Joud Jalal <joudjalal5@gmail.com>
2025-06-14 12:05:18 +0300

    Modifications

--- public/JS/UserProfile.js
+++ public/JS/UserProfile.js
@@ -136,10 +136,10 @@ function HomePageRedirect() {
                 db.collection('Readers').doc(uid).get()
             ]);
 
-            if (authorDoc.exists && !readerDoc.exists) {
-                window.location.href = '/HTML/WriterHomePage.html';
-            } else if (!authorDoc.exists && readerDoc.exists) {
+            if (!authorDoc.exists && readerDoc.exists) {
                 window.location.href = '/HTML/ReaderHomePage.html';
+            } else if (authorDoc.exists && !readerDoc.exists) {
+                window.location.href = '/HTML/WriterHomePage.html';
             } else if (authorDoc.exists && readerDoc.exists) {
                 alert("⚠ يوجد خلل في الحساب: المستخدم موجود ككاتب وقارئ. الرجاء مراجعة الدعم.");
             } else {
